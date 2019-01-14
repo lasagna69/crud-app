518aa4132331a13b9f9766e5c49af32c25ab769a
Thomas Cruz <tom.ortega47@gmail.com>
2019-01-14 17:17:04 +0800

    added routes for create/read/update/delete

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//connecting to remote mongodb
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://User:zkw7tvbr@ds027509.mlab.com:27509/productstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 //imports routes for Products
 const product = require('./routes/product.route');
 
 //initialize express app
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 let port = 1234;
